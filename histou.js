106761cd73461fa5eaa333e7eb6eb88e2c9aa7f0
Griesbacher <philip@griesbacher.org>
2015-09-22 12:15:52 +0200

    removed gaps between lines

--- histou.js
+++ histou.js
@@ -8,6 +8,38 @@ parseArgs()
 
 
 return function (callback) {
+    //Remove Lines over Gap where are no points
+    (function ($) {
+        function init(plot) {
+            function fixGaps(plot, series, datapoints) {
+                    var points = datapoints.points, stepSize = datapoints.pointsize;
+                    var timerangeInMillisec = points[points.length-stepSize]-points[0];
+                    var datapoints = series.data.length;
+                    var avgTimeBetweenPoints = timerangeInMillisec / datapoints;
+                    var indices = [];
+                    for (var i = stepSize; i < points.length; i += stepSize){
+                            if ((points[i]-points[i-stepSize]) > avgTimeBetweenPoints*2){
+                                    indices.push(i)
+                            }
+                    }
+                    for (var i = 0; i < indices.length; i++){
+                            var pointIndex = indices[i];
+                            var oldTimestamp = points[pointIndex];
+                            for (var j = 0; i < stepSize; i++){
+                                    points.splice(pointIndex,0,null);
+                            }
+                    }
+            }
+            plot.hooks.processDatapoints.push(fixGaps);
+        }
+
+        $.plot.plugins.push({
+            init: init,
+            options: {},
+            name: "fixMeasurementGaps",
+            version: "0.1"
+        });
+    })(jQuery);
 
     url = 'http://localhost/histou/index.php';
     configUrl = url+'?host='+host+'&service='+service+'&height='+height+'&legend='+legend+debug;
