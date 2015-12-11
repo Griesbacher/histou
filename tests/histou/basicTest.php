1af0e29f1e776c19f0c854fbc1b95dd7d3567e47
Griesbacher <philip@griesbacher.org>
2015-12-11 15:46:40 +0100

    added tests

--- tests/histou/basicTest.php
+++ tests/histou/basicTest.php
@@ -1,9 +1,32 @@
 <?php
 
-require_once 'histou/basic.php';
+namespace tests;
 
-class BasicTest extends PHPUnit_Framework_TestCase
+class BasicTest extends \PHPUnit_Framework_TestCase
 {
+    public function init()
+    {
+        $_GET['host'] = 'host';
+        \histou\Basic::parsArgs();
+    }
+
+    public function testParseArgs()
+    {
+        $_GET['host'] = "host0";
+        $_GET['service'] = "service0";
+        $_GET['debug'] = "true";
+        $_GET['height'] = "500px";
+        $_GET['legend'] = "false";
+        $_GET['annotations'] = "true";
+        \histou\Basic::parsArgs();
+        $this->assertEquals("host0", HOST);
+        $this->assertEquals("service0", SERVICE);
+        $this->assertEquals(true, \histou\Debug::isEnable());
+        $this->assertEquals("500px", HEIGHT);
+        $this->assertEquals(false, SHOW_LEGEND);
+        $this->assertEquals(true, SHOW_ANNOTATION);
+    }
+
     public function testParseIni()
     {
         \histou\Basic::parsIni('histou.ini.example');
@@ -15,7 +38,7 @@ class BasicTest extends PHPUnit_Framework_TestCase
     public function testSetConstant()
     {
         $object = new \histou\Basic();
-        $reflector = new ReflectionClass('\histou\Basic');
+        $reflector = new \ReflectionClass('\histou\Basic');
         $method = $reflector->getMethod('setConstant');
         $method->setAccessible(true);
 
@@ -29,11 +52,138 @@ class BasicTest extends PHPUnit_Framework_TestCase
     public function testGetConfigKey()
     {
         $object = new \histou\Basic();
-        $reflector = new ReflectionClass('\histou\Basic');
+        $reflector = new \ReflectionClass('\histou\Basic');
         $method = $reflector->getMethod('getConfigKey');
         $method->setAccessible(true);
 
         $result = $method->invokeArgs($object, array(array("foo" => array("bar" => "baz")), "1", "2"));
         $this->assertEquals($result, null);
     }
+
+    public function testReturnData()
+    {
+        $this->init();
+        \histou\Debug::enable();
+        $dashboard = new \histou\grafana\Dashboard('foo');
+        ob_start();
+        \histou\Basic::returnData($dashboard);
+        $out1 = ob_get_contents();
+        ob_end_clean();
+        $this->assertEquals($this->emptyDashboard, $out1);
+
+        $_GET["callback"] = 1;
+        ob_start();
+        \histou\Basic::returnData('{"foo":"bar"}');
+        $out2 = ob_get_contents();
+        ob_end_clean();
+        $this->assertEquals('1({"foo":"bar"})', $out2);
+        
+        ob_start();
+        \histou\Basic::returnData(1);
+        $out3 = ob_get_contents();
+        ob_end_clean();
+        $this->assertEquals("<pre>Don't know what to do with this: 1</pre>", $out3);
+    }
+    private $emptyDashboard = '<pre>Array
+(
+    [id] => 1
+    [title] => foo
+    [originalTitle] => CustomDashboard
+    [tags] => Array
+        (
+        )
+
+    [timezone] => browser
+    [editable] => 1
+    [hideControls] => 1
+    [sharedCrosshair] => 
+    [nav] => Array
+        (
+            [0] => Array
+                (
+                    [type] => timepicker
+                    [enable] => 1
+                    [status] => Stable
+                    [time_options] => Array
+                        (
+                            [0] => 5m
+                            [1] => 15m
+                            [2] => 1h
+                            [3] => 6h
+                            [4] => 12h
+                            [5] => 24h
+                            [6] => 2d
+                            [7] => 7d
+                            [8] => 30d
+                        )
+
+                    [refresh_intervals] => Array
+                        (
+                            [0] => 5s
+                            [1] => 10s
+                            [2] => 30s
+                            [3] => 1m
+                            [4] => 5m
+                            [5] => 15m
+                            [6] => 30m
+                            [7] => 1h
+                            [8] => 2h
+                            [9] => 1d
+                        )
+
+                    [now] => 1
+                    [collapse] => 
+                    [notice] => 
+                )
+
+        )
+
+    [time] => Array
+        (
+            [from] => now-8h
+            [to] => now
+        )
+
+    [templating] => Array
+        (
+        )
+
+    [annotations] => Array
+        (
+            [list] => Array
+                (
+                )
+
+        )
+
+    [refresh] => 5s
+    [version] => 6
+    [rows] => Array
+        (
+            [0] => Array
+                (
+                    [titel] => Debug
+                    [editable] => 1
+                    [height] => 400px
+                    [panels] => Array
+                        (
+                            [0] => Array
+                                (
+                                    [title] => 
+                                    [type] => text
+                                    [span] => 12
+                                    [editable] => 1
+                                    [id] => 1
+                                    [mode] => markdown
+                                    [content] => 
+                                )
+
+                        )
+
+                )
+
+        )
+
+)
+<br>0<br>{"id":"1","title":"foo","originalTitle":"CustomDashboard","tags":[],"timezone":"browser","editable":true,"hideControls":true,"sharedCrosshair":false,"nav":[{"type":"timepicker","enable":true,"status":"Stable","time_options":["5m","15m","1h","6h","12h","24h","2d","7d","30d"],"refresh_intervals":["5s","10s","30s","1m","5m","15m","30m","1h","2h","1d"],"now":true,"collapse":false,"notice":false}],"time":{"from":"now-8h","to":"now"},"templating":[],"annotations":{"list":[]},"refresh":"5s","version":"6","rows":[{"titel":"Debug","editable":true,"height":"400px","panels":[{"title":"","type":"text","span":12,"editable":true,"id":1,"mode":"markdown","content":""}]}]}<br></pre>';
 }
--- tests/histou/debugTest.php
+++ tests/histou/debugTest.php
@@ -1,11 +1,58 @@
 <?php
 
-require_once 'histou/debug.php';
+namespace tests;
 
-class DebugTest extends PHPUnit_Framework_TestCase
+class DebugTest extends \PHPUnit_Framework_TestCase
 {
+    public function init()
+    {
+        $_GET['host'] = 'host';
+        \histou\Basic::parsArgs();
+    }
+
     public function testEnable()
     {
+        $this->assertEquals(\histou\Debug::isEnable(), false);
+        \histou\Debug::enable();
+        $this->assertEquals(\histou\Debug::isEnable(), true);
+    }
+
+    public function testPrintBoolean()
+    {
+        $this->assertEquals(\histou\Debug::printBoolean(true), "true");
+        $this->assertEquals(\histou\Debug::printBoolean(false), "false");
+    }
+
+    public function testGetLogAsMarkdown()
+    {
+        \histou\Debug::add("foo");
+        $this->assertEquals("#### foo\n", \histou\Debug::getLogAsMarkdown());
+        \histou\Debug::add("bar");
+        $this->assertEquals("#### foo\n#### bar\n", \histou\Debug::getLogAsMarkdown());
+    }
+
+    public function testGenMarkdownRow()
+    {
+        $this->init();
+        $panel = new \histou\grafana\TextPanel('', 1);
+        $panel->setMode(\histou\grafana\TextPanel::MARKDOWN);
+        $panel->setContent("foo");
+        $row = new \histou\grafana\Row("bar");
+        $row->addPanel($panel);
+        $this->assertEquals($row, \histou\Debug::genMarkdownRow("foo", "bar"));
+    }
+
+    public function testErrorMarkdownDashboard()
+    {
+        $this->init();
+        $panel = new \histou\grafana\TextPanel('', 1);
+        $panel->setMode(\histou\grafana\TextPanel::MARKDOWN);
+        $panel->setContent("foo");
+        $row = new \histou\grafana\Row("ERROR");
+        $row->addPanel($panel);
+        $dashboard = new \histou\grafana\Dashboard('Error');
+        $dashboard->addRow($row);
+        $this->assertEquals($dashboard, \histou\Debug::errorMarkdownDashboard("foo"));
 
     }
 }
