fc99c33217996a3cfac528b6603e95946b1c403d
Griesbacher <philip@griesbacher.org>
2015-12-16 11:52:41 +0100

    added tests

--- tests/histou/template/templateTest.php
+++ tests/histou/template/templateTest.php
@@ -13,8 +13,8 @@ class TemplateTest extends \MyPHPUnitFrameworkTestCase
     public function testMatchesTablename()
     {
         $t = new \histou\template\Template(
-            'file',
-            new \histou\template\Rule('host', 'service', 'command', array('p1','p2'), 'file'),
+            '',
+            new \histou\template\Rule('host', 'service', 'command', array('p1','p2'), ''),
             function ($perfData) {
                 return 'foo';
             }
@@ -25,17 +25,15 @@ class TemplateTest extends \MyPHPUnitFrameworkTestCase
         $this->assertSame(false, $t->matchesTablename('host&service&command&p3&value'));
     }
 
-
-
     public function testValidTemplate()
     {
         $validTests = array(
-            array(new \histou\template\Rule('host', 'service', 'command', array(), 'file'), false),
-            array(new \histou\template\Rule('host', 'service', 'command', array('p1', 'p2'), 'file'), false),
-            array(new \histou\template\Rule('host', 'service', 'command', array('p1', 'p2', 'p3'), 'file'), true),
-            array(new \histou\template\Rule('host', 'service', 'command', array('p1', 'p2', 'p3', 'p4'), 'file'), false),
-            array(new \histou\template\Rule('host', 'service', 'command', array('.*'), 'file'), true),
-            array(new \histou\template\Rule('host', 'service', 'foo', array('.*'), 'file'), false),
+            array(new \histou\template\Rule('host', 'service', 'command', array(), ''), false),
+            array(new \histou\template\Rule('host', 'service', 'command', array('p1', 'p2'), ''), false),
+            array(new \histou\template\Rule('host', 'service', 'command', array('p1', 'p2', 'p3'), ''), true),
+            array(new \histou\template\Rule('host', 'service', 'command', array('p1', 'p2', 'p3', 'p4'), ''), false),
+            array(new \histou\template\Rule('host', 'service', 'command', array('.*'), ''), true),
+            array(new \histou\template\Rule('host', 'service', 'foo', array('.*'), ''), false),
         );
         \histou\template\Rule::setCheck(
             'host',
@@ -45,14 +43,123 @@ class TemplateTest extends \MyPHPUnitFrameworkTestCase
         );
 
         foreach ($validTests as $test) {
-            $t = new \histou\template\Template(
-                'file',
-                $test[0],
+            $this->assertSame($test[1], static::createTemplate($test[0])->isValid());
+        }
+    }
+    public function testCompareTemplate()
+    {
+		$compareTests = array(
+			array(
+				'expected' => new \histou\template\Rule('host', 'service', 'command', array(), ''),
+				'input' => array(
+									new \histou\template\Rule('host', 'service', 'command', array(), '')
+								)
+			),
+			//start winns over no match
+			array(
+				'expected' => new \histou\template\Rule('host', 'service', 'command', array('.*'), ''),
+				'input' => array(
+									new \histou\template\Rule('host', 'service', 'command', array('p1'), ''),
+									new \histou\template\Rule('host', 'service', 'command', array('p2'), ''),
+									new \histou\template\Rule('host', 'service', 'command', array('.*'), ''),
+									new \histou\template\Rule('host', 'service', 'command', array('p3'), ''),
+								)
+			),
+			//exactmatch wins over star
+			array(
+				'expected' => new \histou\template\Rule('host', 'service', 'command', array('p1', 'p2', 'p3'), ''),
+				'input' => array(
+									new \histou\template\Rule('host', 'service', 'command', array('.*'), ''),
+									new \histou\template\Rule('host', 'service', 'command', array('p1', 'p2', 'p3'), ''),
+								)
+			),
+			//exactmatch wins over star
+			array(
+				'expected' => new \histou\template\Rule('host', 'service', 'command', array('p1', 'p2', 'p3'), ''),
+				'input' => array(
+									new \histou\template\Rule('host', 'service', 'command', array('p1', 'p2', 'p3'), ''),
+									new \histou\template\Rule('host', 'service', 'command', array('.*'), ''),
+								)
+			),
+			//equals
+			array(
+				'expected' => new \histou\template\Rule('host', 'service', 'command', array('p1', 'p2', 'p3'), ''),
+				'input' => array(
+									new \histou\template\Rule('host', 'service', 'command', array('p1', 'p2', 'p3'), ''),
+									new \histou\template\Rule('host', 'service', 'command', array('p1', 'p2', 'p3'), ''),
+									new \histou\template\Rule('host', 'service', 'command', array('.*'), ''),
+								)
+			),
+			//command over perfLabel
+			array(
+				'expected' => new \histou\template\Rule('host', 'service', 'command', array('.*'), ''),
+				'input' => array(
+									new \histou\template\Rule('host', 'service', 'foo', array('p1', 'p2', 'p3'), ''),
+									new \histou\template\Rule('host', 'service', 'command', array('.*'), ''),
+								)
+			),
+			//too much infos
+			array(
+				'expected' => new \histou\template\Rule('host', 'service', 'command', array('p1', 'p2', 'p3'), ''),
+				'input' => array(
+									new \histou\template\Rule('host', 'service', 'command', array('p1', 'p2', 'p3', 'p4'), ''),
+									new \histou\template\Rule('host', 'service', 'command', array('p1', 'p2', 'p3'), ''),
+									new \histou\template\Rule('host', 'service', 'command', array('p1', 'p2'), ''),
+								)
+			),
+			//too much infos desc
+			array(
+				'expected' => new \histou\template\Rule('host', 'service', 'command', array('p1', 'p2', 'p3'), ''),
+				'input' => array(
+									new \histou\template\Rule('host', 'service', 'command', array('p1', 'p2'), ''),
+									new \histou\template\Rule('host', 'service', 'command', array('p1', 'p2', 'p3'), ''),
+									new \histou\template\Rule('host', 'service', 'command', array('p1', 'p2', 'p3', 'p4'), ''),
+								)
+			),
+		);
+
+		\histou\template\Rule::setCheck(
+            'host',
+            'service',
+            'command',
+            array('p1', 'p2', 'p3')
+        );
+
+		foreach ($compareTests as $test) {
+			$templates = array();
+			foreach($test['input'] as $rule) {
+				array_push($templates, static::createTemplate($rule));
+			}
+			usort($templates, '\histou\template\Template::compare');
+
+			$this->assertEquals($test['expected'], $templates[0]->getRule());
+		}
+
+		foreach ($compareTests as $test) {
+			$rules = array();
+			foreach($test['input'] as $rule) {
+				array_push($rules, $rule);
+			}
+			usort($rules, '\histou\template\Template::compare');
+
+			$this->assertEquals($test['expected'], $rules[0]);
+		}
+	}
+
+	public function testFailCompareTemplate()
+    {
+		$this->setExpectedException('InvalidArgumentException');
+		$templates = array(new \histou\template\loader(), new \histou\template\loader());
+		usort($templates, '\histou\template\Template::compare');
+	}
+
+	private static function createTemplate($rule) {
+		return new \histou\template\Template(
+                '',
+                $rule,
                 function ($perfData) {
                     return 'foo';
                 }
             );
-            $this->assertSame($test[1], $t->isValid());
-        }
-    }
+	}
 }
