1bec1d74e5012c7220472a3b028be383dbaa37a4
Philip Griesbacher <philip@griesbacher.org>
2016-11-30 13:48:18 +0100

    added new modes

--- index.php
+++ index.php
@@ -41,71 +41,77 @@ header("access-control-allow-origin: *");
 ini_set('default_socket_timeout', DEFAULT_SOCKET_TIMEOUT);
 
 
-// database load perfdata
-$perfData = null;
-$database = null;
-if (DATABASE_TYPE == INFLUXDB) {
-	$database = new \histou\database\Influxdb(URL);
-}elseif(DATABASE_TYPE == ELASTICSEARCH){
-	$database = new \histou\database\Elasticsearch(URL);
-}else{
-	\histou\Basic::returnData(\histou\Debug::errorMarkdownDashboard('# Unsupported database'), 1);
-}
+$perfData = array('host' => HOST, 'service' => SERVICE);
+if (!\histou\Basic::$disablePerfdataLookup){
+	// database load perfdata
+	$database = null;
+	if (DATABASE_TYPE == INFLUXDB) {
+		$database = new \histou\database\Influxdb(URL);
+	}elseif(DATABASE_TYPE == ELASTICSEARCH){
+		$database = new \histou\database\Elasticsearch(URL);
+	}else{
+		\histou\Basic::returnData(\histou\Debug::errorMarkdownDashboard('# Unsupported database'), 1);
+	}
 
-$request = $database->fetchPerfData();
-if (empty($request)) {
-	\histou\Basic::returnData(\histou\Debug::errorMarkdownDashboard('# Database not reachable or empty result'), 1);
-	exit(0);
-}
-$perfData = $database->filterPerfdata(
-	$request,
-	HOST,
-	SERVICE
-);
+	$request = $database->fetchPerfData();
+	if (empty($request)) {
+		\histou\Basic::returnData(\histou\Debug::errorMarkdownDashboard('# Database not reachable or empty result'), 1);
+		exit(0);
+	}
+	$perfData = $database->filterPerfdata(
+		$request,
+		HOST,
+		SERVICE
+	);
 
-$perfDataSize = sizeof($perfData);
-if ($perfDataSize < 4) {
-    if ($perfDataSize == 1) {
-        \histou\Basic::returnData(\histou\Debug::errorMarkdownDashboard('# Database Error: '.$perfData), 1);
-		exit(1);
-    } else {
-        \histou\Basic::returnData(\histou\Debug::errorMarkdownDashboard('# Host / Service not found in Database'), 1);
-		exit(1);
-    }
+	$perfDataSize = sizeof($perfData);
+	if ($perfDataSize < 4) {
+		if ($perfDataSize == 1) {
+			\histou\Basic::returnData(\histou\Debug::errorMarkdownDashboard('# Database Error: '.$perfData), 1);
+			exit(1);
+		} else {
+			\histou\Basic::returnData(\histou\Debug::errorMarkdownDashboard('# Host / Service not found in Database'), 1);
+			exit(1);
+		}
+	}
 }
-//save databaseresult to rule
-\histou\template\Rule::setCheck(
-    $perfData['host'],
-    $perfData['service'],
-    $perfData['command'],
-    array_keys($perfData['perfLabel'])
-);
 
 // load templates
 $templateFiles = \histou\Folder::loadFolders(
-    array(CUSTOM_TEMPLATE_FOLDER, DEFAULT_TEMPLATE_FOLDER)
+	array(CUSTOM_TEMPLATE_FOLDER, DEFAULT_TEMPLATE_FOLDER)
 );
-
 $templateCache = new \histou\template\cache('templates');
 $templates = $templateCache->loadTemplates($templateFiles, '\histou\template\loader::loadTemplate');
-
 if (sizeof($templates) == 0) {
-    \histou\Basic::returnData(\histou\Debug::errorMarkdownDashboard('# Could not load templates!'), 1);
+	\histou\Basic::returnData(\histou\Debug::errorMarkdownDashboard('# Could not load templates!'), 1);
 	exit(1);
 }
 
-usort($templates, '\histou\template\Template::compare');
-$valid = $templates[0]->isValid();
-\histou\Debug::add("Template order:");
-foreach ($templates as $template) {
-    \histou\Debug::add($template);
-}
-\histou\Debug::add("Is the first template valid: ".\histou\Debug::printBoolean($valid)."\n");
-\histou\Debug::add("Data: ".print_r($perfData, true));
-if ($valid) {
-    $template = $templates[0];
-} else {
-    $template = \histou\template\Template::findDefaultTemplate($templates, 'default.php');
+if (\histou\Basic::$specificTemplate == ""){
+	// search for the best	
+	//save databaseresult to rule
+	\histou\template\Rule::setCheck(
+		$perfData['host'],
+		$perfData['service'],
+		$perfData['command'],
+		array_keys($perfData['perfLabel'])
+	);
+	usort($templates, '\histou\template\Template::compare');
+	$valid = $templates[0]->isValid();
+	\histou\Debug::add("Template order:");
+	foreach ($templates as $template) {
+		\histou\Debug::add($template);
+	}
+	\histou\Debug::add("Is the first template valid: ".\histou\Debug::printBoolean($valid)."\n");
+	\histou\Debug::add("Data: ".print_r($perfData, true));
+	if ($valid) {
+		$template = $templates[0];
+	} else {
+		$template = \histou\template\Template::findDefaultTemplate($templates, 'default.php');
+	}
+}else{
+	// find the one
+	$template = \histou\template\Template::findDefaultTemplate($templates, \histou\Basic::$specificTemplate);
 }
 
 // load ForecastTemplates
