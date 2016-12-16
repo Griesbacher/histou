3ec9414ae24b77a6a62fe58c37ee77d901e7b692
Philip Griesbacher <philip@griesbacher.org>
2016-12-16 12:20:07 +0100

    fixed typo

--- histou.js
+++ histou.js
@@ -13,7 +13,7 @@ return function (callback) {
     }
 
     var url = 'http://localhost/histou/';
-    var configUrl = url+'index.php?host='+host+'&service='+service+'&height='+height+'&legend='+legend+debug+disablePanelTitel+disablePerfdataLookup+specificTemplate+'&annotations='+annotations;
+    var configUrl = url+'index.php?host='+host+'&service='+service+'&height='+height+'&legend='+legend+debug+disablePanelTitle+disablePerfdataLookup+specificTemplate+'&annotations='+annotations;
 
     var flotAddons = url + 'flotAddons.js';
     $.getScript(flotAddons, function (){});
