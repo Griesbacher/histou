2010fee8941e4715f4a3e10a1e57dbabfbdaa32f
Rémi VERCHERE <remi.verchere@axians.com>
2019-09-03 17:47:58 +0200

    Specify port in histou URL, if not listening on default ports

--- histou.js
+++ histou.js
@@ -12,7 +12,7 @@ return function (callback) {
         document.documentElement.style.background = '#FFF';
     }
 
-    var url = location.protocol+'//'+window.location.hostname+'/histou/';
+    var url = location.protocol+'//'+window.location.hostname+':'+window.location.port+'/histou/';
     var configUrl = url+'index.php?host='+host+'&service='+service+'&height='+height+'&legend='+legend+debug+disablePanelTitle+disablePerfdataLookup+specificTemplate+'&annotations='+annotations;
 
     var flotAddons = url + 'flotAddons.js';
